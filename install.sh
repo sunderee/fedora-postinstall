786ee9344c82b8d554bb65f42b78f58f12097b62
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2021-11-05 17:35:52 +0100

    Add script for installing Brave, and useful snaps

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo dnf install snapd -y
 sudo ln -s /var/lib/snapd/snap /snap
 
 # Install Flat Remix theme
-sudo dnf copr enable daniruiz/flat-remix
+sudo dnf copr enable daniruiz/flat-remix -y
 sudo dnf install flat-remix-gnome flat-remix-icon-theme flat-remix-gnome gnome-tweaks -y
 
 # Install Node.JS and NPM
@@ -16,4 +16,10 @@ sudo npm install nodejs -y
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 dnf check-update
-sudo dnf install code
+sudo dnf install code -y
+
+# Install Brave Browser
+sudo dnf install dnf-plugins-core -y
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser -y
---
+++ snaps.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Development: Flutter, Postman
+sudo snap install flutter --classic
+sudo snap install postman
+
+# Entertainment: Discord, Spotify
+sudo snap install discord
+sudo snap install spotify
+
+# Tools: Bitwarden, Slack, Mailspring, Typora
+sudo snap install bitwarden
+sudo snap install slack --classic
+sudo snap install mailspring
+sudo snap install typora
