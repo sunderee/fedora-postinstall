5b5bd0976071947a33beb1de763c56f446af04a3
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2021-11-05 17:26:27 +0100

    Add instructions for installing VSCode

--- install.sh
+++ install.sh
@@ -11,3 +11,9 @@ sudo dnf install flat-remix-gnome flat-remix-icon-theme flat-remix-gnome gnome-t
 
 # Install Node.JS and NPM
 sudo npm install nodejs -y
+
+# Install VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install code
